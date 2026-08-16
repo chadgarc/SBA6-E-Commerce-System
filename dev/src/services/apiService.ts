fa4b4bf8277c75fb30fe22a17cabb9590e9e3697
Christian Garlen <chadgarval@outlook.com>
2026-08-15 22:47:38 -0400

    Added methods to retrieve inventory, search products, get inventory quantity, and get product by Id

---
+++ dev/src/services/apiService.ts
@@ -0,0 +1,125 @@
+import { DataError, IdError } from "../utils/errorHandler";
+import { Product, type ProductData } from "../models/Product";
+
+enum PRODUCTS{
+    URL = 'https://dummyjson.com/products'
+}
+
+export const getInventory = async (limit: number = 30) => {
+
+    try{
+        const response = await fetch(`${PRODUCTS.URL}?limit=${limit}`);
+
+        if(!response.ok){
+            throw new DataError("Unable to retrieve data");
+        }
+
+        const data = await response.json();
+
+        const inventory: ProductData[] = data.products.map((product: ProductData) => {
+            return new Product(
+                product.id,
+                product.title,
+                product.price,
+                product.discountPercentage,
+                product.category
+            );
+        })
+
+        return inventory;
+    }
+    catch(error){
+        if(error instanceof DataError){
+            console.error("Data Error:", error.message);
+        } else{
+            console.error(error);
+        }
+    }
+}
+
+export const searchProduct = async (target: string) => {
+
+    try{
+        const response = await fetch(`${PRODUCTS.URL}/search?q=${target}`);
+
+        if(!response.ok){
+            throw new DataError("Unable to retrieve Data");
+        }
+
+        const data = await response.json();
+
+        const inventory: ProductData[] = data.products.map((product: ProductData) => {
+            return new Product(
+                product.id,
+                product.title,
+                product.price,
+                product.discountPercentage,
+                product.category
+            );
+        })
+
+        return inventory;
+    }
+    catch(error){
+        if(error instanceof DataError){
+            console.error("Not found:", error.message);
+        } else{
+            console.error(error);
+        }
+    }
+}
+
+export const getInventoryQty = async () => {
+
+    try{
+        const response = await fetch(PRODUCTS.URL);
+
+        if(!response.ok){
+            throw new DataError("Unable to retrieve data");
+        }
+
+        const data = await response.json();
+
+        const inventory: number = data.total
+
+        return inventory;
+    }
+    catch(error){
+        if(error instanceof DataError){
+            console.error("Data Error:", error.message);
+        } else{
+            console.error(error);
+        }
+    }
+}
+
+export const getProductById = async (id: number) => {
+
+    try{
+
+        const response = await fetch(`${PRODUCTS.URL}/${id}`);
+
+        if(!response.ok){
+            throw new IdError("ID doesn't exist");
+        }
+
+        const data = await response.json();
+
+        const product: ProductData = new Product(
+            data.id,
+            data.title,
+            data.price,
+            data.discountPercentage,
+            data.category
+        );
+
+        return product;
+
+    } catch(error){
+        if( error instanceof IdError){
+            console.error("Data Error:", error.message);
+        } else{
+            console.error(error);
+        }
+    }
+}
