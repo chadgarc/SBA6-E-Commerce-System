9c2f8d1dd2fb387be2f6f77094ef3e91118ba3bf
Christian Garlen <chadgarval@outlook.com>
2026-08-15 23:14:44 -0400

    Added methods to retrieve products by category, get all categories, sort products

--- dev/src/services/apiService.ts
+++ dev/src/services/apiService.ts
@@ -5,10 +5,101 @@ enum PRODUCTS{
     URL = 'https://dummyjson.com/products'
 }
 
+/**
+ *
+ * THIS SECTION RETRIEVES DATA FROM API
+ *
+ * */
+
 export const getInventory = async (limit: number = 30) => {
 
     try{
         const response = await fetch(`${PRODUCTS.URL}?limit=${limit}`);
+
+        if(!response.ok){
+            throw new DataError("Unable to retrieve data");
+        }
+
+        const data = await response.json();
+
+        const inventory: ProductData[] = data.products.map((product: ProductData) => {
+            return new Product(
+                product.id,
+                product.title,
+                product.price,
+                product.discountPercentage,
+                product.category
+            );
+        })
+
+        return inventory;
+    }
+    catch(error){
+        if(error instanceof DataError){
+            console.error("Data Error:", error.message);
+        } else{
+            console.error(error);
+        }
+    }
+}
+
+export const getAllCategories = async () => {
+
+    try{
+        const response = await fetch(`${PRODUCTS.URL}/category-list`);
+
+        if(!response.ok){
+            throw new DataError("Unable to retrieve data");
+        }
+
+        return response.json();
+    }
+    catch(error){
+        if(error instanceof DataError){
+            console.error("Data Error:", error.message);
+        } else{
+            console.error(error);
+        }
+    }
+}
+
+export const getProductsByCategory = async (category: string) => {
+
+    try{
+        const response = await fetch(`${PRODUCTS.URL}/category/${category}`);
+
+        if(!response.ok){
+            throw new DataError("Unable to retrieve data");
+        }
+
+        const data = await response.json();
+
+        const inventory: ProductData[] = data.products.map((product: ProductData) => {
+            return new Product(
+                product.id,
+                product.title,
+                product.price,
+                product.discountPercentage,
+                product.category
+            );
+        })
+
+        return inventory;
+    }
+    catch(error){
+        if(error instanceof DataError){
+            console.error("Data Error:", error.message);
+        } else{
+            console.error(error);
+        }
+    }
+}
+
+// asc or desc
+export const sortProducts = async ( { target = 'title', order = 'asc', limit = 30 }) => {
+
+    try{
+        const response = await fetch(`${PRODUCTS.URL}?sortBy=${target}&order=${order}&limit=${limit}`);
 
         if(!response.ok){
             throw new DataError("Unable to retrieve data");
@@ -80,9 +171,7 @@ export const getInventoryQty = async () => {
 
         const data = await response.json();
 
-        const inventory: number = data.total
-
-        return inventory;
+        return data.total;
     }
     catch(error){
         if(error instanceof DataError){
